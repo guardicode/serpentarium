c86effda95ffc1d93fb814b5a873f4f50f54adf8
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-10 10:26:44 -0500

    Save the state of sys.modules on import

--- serpentarium/__init__.py
+++ serpentarium/__init__.py
@@ -1,2 +1,8 @@
+# This needs to be the first thing imported in the host program so that we can save the state of the
+# import system before it gets modified.
+import sys
+
+CLEAN_SYS_MODULES = sys.modules.copy()
+
 from .plugin import Plugin
 from .abstract_plugin import AbstractPlugin
