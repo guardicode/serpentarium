37ed99521e5d1b97435f1171fc9b0aa239e3b8c9
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-10 14:47:14 -0500

    Test constructor and run() parameters

--- tests/test_plugin_loader.py
+++ tests/test_plugin_loader.py
@@ -5,6 +5,7 @@ import pytest
 from serpentarium import PluginLoader
 
 PLUGIN_DIR = Path(__file__).parent / "plugins"
+MY_PARAM = "test_param"
 
 
 @pytest.fixture
@@ -28,3 +29,19 @@ def test_module_not_found_incorrect_plugin_name(plugin_loader: PluginLoader):
     with pytest.raises(ModuleNotFoundError):
         plugin = plugin_loader.load(plugin_name="NONEXISTANT")
         plugin.run()
+
+
+def test_constructor_parameters(plugin_loader: PluginLoader):
+    plugin1 = plugin_loader.load(plugin_name="constructor_parameters", my_param=MY_PARAM)
+
+    return_value = plugin1.run()
+
+    assert return_value == MY_PARAM
+
+
+def test_run_parameters(plugin_loader: PluginLoader):
+    plugin1 = plugin_loader.load(plugin_name="run_parameters")
+
+    return_value = plugin1.run(my_param=MY_PARAM)
+
+    assert return_value == MY_PARAM
