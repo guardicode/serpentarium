4ad0d12a86901a71108a0303e19c490597ed3aa4
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-11 20:41:20 -0500

    Remove disused Plugin Protocol

--- serpentarium/plugin.py
+++ serpentarium/plugin.py
@@ -3,18 +3,6 @@ from typing import Any
 from typing_extensions import Protocol
 
 
-class Plugin(Protocol):
-    """A protocol for plugins"""
-
-    def run(self, **kwargs) -> Any:
-        """Run a plugin with the provided keyword arguments and returns the result"""
-        pass
-
-    @property
-    def name(self) -> str:
-        """A name that identifies this plugin"""
-
-
 class SingleUsePlugin(Protocol):
     """A protocol for a plugin that can be run only once"""
 
