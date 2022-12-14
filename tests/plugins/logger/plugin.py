52fe2b5063bb7e42628920d3272c88e0c59acb73
Mike Salvatore <mike.s.salvatore@gmail.com>
2022-12-13 19:50:31 -0500

    Remove dependency on defunct AbstractPlugin from test logging plugin

--- tests/plugins/logger/plugin.py
+++ tests/plugins/logger/plugin.py
@@ -1,12 +1,12 @@
 import logging
 from typing import Iterable, Tuple
 
-from serpentarium import AbstractPlugin
+from serpentarium import MultiUsePlugin, NamedPluginMixin
 
 logger = logging.getLogger("LoggingPlugin")
 
 
-class Plugin(AbstractPlugin):
+class Plugin(NamedPluginMixin, MultiUsePlugin):
     def run(self, log_messages: Iterable[Tuple[int, str]] = []):  # type: ignore[override]
         for msg in log_messages:
             logger.log(msg[0], msg[1])
